3e8392e94578cb2d612dab3336f1e88d97967cb0
Valeriy Andrushchak <vandrushchak@spiria.com>
2025-02-09 12:55:16 -0500

    Update

--- apps/frontend/src/app/setup/add-username.tsx
+++ apps/frontend/src/app/setup/add-username.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
 interface AddUsernameProps {
   setCurrentStep: (step: string) => void;
@@ -5,28 +6,31 @@ interface AddUsernameProps {
 
 export const AddUsername = ({setCurrentStep}: AddUsernameProps) => {
   const username = localStorage.getItem('username');
+  const [value, setValue] = useState(username || '');
   const handleAddUsername = () => {
+    localStorage.setItem('username', value);
     setCurrentStep('chooseRole');
   };
   const { t } = useTranslation();
   return (
     <div className="text-center">
     <p className="text-lg text-gray-700">
-      {username ? t('updateUsername') : t('addUsername')}
+      {username || value ? t('updateUsername') : t('addUsername')}
     </p>
     <input
       type="text"
       className="px-4 py-2 border border-gray-300 rounded-lg"
-      placeholder="Enter your username"
-      defaultValue={username || ''}
+      placeholder={t('enterName')}
+      defaultValue={value}
       onChange={(e) => {
-        localStorage.setItem('username', e.target.value);
+        setValue(e.target.value);
       }}
     />
     <button
       type="submit"
-      className="px-6 py-2 bg-green-600 text-white rounded-lg shadow-md hover:bg-green-700 transition duration-300"
+      className="px-6 py-2 bg-green-600 text-white rounded-lg shadow-md hover:bg-green-700 transition duration-300 disabled:bg-slate-500 disabled:cursor-not-allowed"
       onClick={handleAddUsername}
+      disabled={!value}
     >
       {t('continue')}
     </button>
